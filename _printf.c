2669bacbc677ceeb3889b2d21aa0493473b62c07
Charooo05 <charleschuks99@gmail.com>
2022-10-17 08:48:25 -0700

    group task

--- _printf.c
+++ _printf.c
@@ -1,121 +1,92 @@
 #include "main.h"
 
 void cleanup(va_list args, buffer_t *output);
-
 int run_printf(const char *format, va_list args, buffer_t *output);
-
 int _printf(const char *format, ...);
 
+/**
+ * cleanup - cleanup
+ * @args: variadic list
+ * @output: output to stdout
+ */
 void cleanup(va_list args, buffer_t *output)
-
 {
+	va_end(args);
 
-va_end(args);
-
-write(1, output->start, output->len);
-
-free_buffer(output);
-
+	write(1, output->start, output->len);
+	free_buffer(output);
 }
 
+/**
+ * run_printf - run_printf
+ * @format: a character string
+ * @args: variadic list
+ * @output: output to stdout
+ * Return: number of characters
+ */
 int run_printf(const char *format, va_list args, buffer_t *output)
-
 {
+	int i, wid, prec;
+	int ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t*, unsigned char, int, int, unsigned char);
 
-int i, wid, prec, ret = 0;
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+			&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+			f = handle_specifiers(format + i + tmp + 1);
 
-char tmp;
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
 
-unsigned char flags, len;
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
 
-unsigned int (*f)(va_list, buffer_t *,
+	cleanup(args, output);
 
-unsigned char, int, int, unsigned char);
-
-for (i = 0; *(format + i); i++)
-
-{
-
-len = 0;
-
-if (*(format + i) == '%')
-
-{
-
-tmp = 0;
-
-flags = handle_flags(format + i + 1, &tmp);
-
-wid = handle_width(args, format + i + tmp + 1, &tmp);
-
-prec = handle_precision(args, format + i + tmp + 1,
-
-&tmp);
-
-len = handle_length(format + i + tmp + 1, &tmp);
-
-f = handle_specifiers(format + i + tmp + 1);
-
-if (f != NULL)
-
-{
-
-i += tmp + 1;
-
-ret += f(args, output, flags, wid, prec, len);
-
-continue;
-
+	return (ret);
 }
 
-else if (*(format + i + tmp + 1) == '\0')
-
-{
-
-ret = -1;
-
-break;
-
-}
-
-}
-
-ret += _memcpy(output, (format + i), 1);
-
-i += (len != 0) ? 1 : 0;
-
-}
-
-cleanup(args, output);
-
-return (ret);
-
-}
-
+/**
+ * _printf - produces output according to a format
+ * @format: a character string
+ * Return: the number of characters printed minus '\0'
+ */
 int _printf(const char *format, ...)
-
 {
+	buffer_t *output;
+	va_list args;
+	int ret;
 
-buffer_t *output;
+	if (format == NULL)
+		return (-1);
 
-va_list args;
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
-int ret;
+	va_start(args, format);
+	ret = run_printf(format, args, output);
 
-if (format == NULL)
-
-return (-1);
-
-output = init_buffer();
-
-if (output == NULL)
-
-return (-1);
-
-va_start(args, format);
-
-ret = run_printf(format, args, output);
-
-return (ret);
-
+	return (ret);
 }
